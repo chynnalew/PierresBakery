8fe759ebef8987eaa34b65477618291420e5345b
chynnalew <chynnalew@yahoo.com>
2021-09-24 08:51:11 -0700

    pass Bread creation, one loaf price tests

--- PierresBakery.Tests/ModelTests/GoodsTests.cs
+++ PierresBakery.Tests/ModelTests/GoodsTests.cs
@@ -1,16 +1,25 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PierresBakery.Models;
+using System;
+using System.Collections.Generic;
 
-namespace Goods.Tests
+namespace PierresBakery.Tests
 {
   [TestClass]
-  public class ItemTests
+  public class BreadTests
   {
     [TestMethod]
-    public void NameOfMethod_DescribeBehavior_ExpectedResult()
+    public void Bread_CreateNewInstanceOfBread_Bread()
     {
-
+      Bread testBread = new Bread();
+      Assert.AreEqual(typeof(Bread), testBread.GetType());
     }
-
+    
+    [TestMethod]
+    public void GetPrice_ReturnsPriceOfOneLoafAsOne_Int()
+    {
+      Bread testBread = new Bread();
+      Assert.AreEqual(5, testBread.GetPrice("1")); 
+    }
   }
 }
--- PierresBakery/Models/Goods.cs
+++ PierresBakery/Models/Goods.cs
@@ -1,9 +1,13 @@
 namespace PierresBakery.Models
 {
-  public class Goods
+  public class Bread
   {
-
-    // properties, methods, etc. will go here.
-
+    public int GetPrice(string amountOfBread)
+    {
+      int breadPrice = 5;
+      int amountOfBreadInt = int.Parse(amountOfBread);
+      int totalCost = breadPrice*amountOfBreadInt;
+      return totalCost;
+    }
   }
 }
